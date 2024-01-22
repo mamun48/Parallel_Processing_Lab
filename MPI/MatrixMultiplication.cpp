6c68bc500421b96276e4a56f183ee1b601e06003
Mamun Miah <mamunmiah193gsc@gmail.com>
2024-01-22 23:25:22 +0600

    Phonebook updated

--- MPI/MatrixMultiplication.cpp
+++ MPI/MatrixMultiplication.cpp
@@ -12,47 +12,47 @@ int main(int argc, char **argv)
     int Mat_row;
     int Mat_col;
     int MatB_col;
-    if(rank == 0)
+    if (rank == 0)
     {
         printf("Enter Number of matrices\n");
-        scanf("%d",&Num_matrices);
+        scanf("%d", &Num_matrices);
         printf("Enter Number of row of Mat A\n");
-        scanf("%d",&Mat_row);
+        scanf("%d", &Mat_row);
         printf("Enter Number of row of Mat B\n");
-        scanf("%d",&Mat_col);
+        scanf("%d", &Mat_col);
         printf("Enter Number of colum of Mat B\n");
-        scanf("%d",&MatB_col);
+        scanf("%d", &MatB_col);
     }
 
-    //Announcing the value of four variable to each core using MPI_Bcast
-    MPI_Bcast(&Num_matrices,1,MPI_INT,0,MPI_COMM_WORLD);
-    MPI_Bcast(&Mat_row,1,MPI_INT,0,MPI_COMM_WORLD);
-    MPI_Bcast(&Mat_col,1,MPI_INT,0,MPI_COMM_WORLD);
-    MPI_Bcast(&MatB_col,1,MPI_INT,0,MPI_COMM_WORLD);
+    // Announcing the value of four variable to each core using MPI_Bcast
+    MPI_Bcast(&Num_matrices, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&Mat_row, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&Mat_col, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&MatB_col, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     int root = 0;
 
-    //Declare the Matrix A,B and result
+    // Declare the Matrix A,B and result
     int matricesA[Num_matrices][Mat_row][Mat_col];
     int matricesB[Num_matrices][Mat_col][MatB_col];
     srand(time(0));
 
-    //Take input using random function
-    if(rank == root)
+    // Take input using random function
+    if (rank == root)
     {
-        for(int i = 0; i < Num_matrices; i++)
+        for (int i = 0; i < Num_matrices; i++)
         {
-            for(int row = 0; row < Mat_row; row++)
+            for (int row = 0; row < Mat_row; row++)
             {
-                for(int colum = 0; colum < Mat_col; colum++)
+                for (int colum = 0; colum < Mat_col; colum++)
                 {
                     matricesA[i][row][colum] = rand() % 10;
                 }
             }
 
-            for(int row = 0; row < Mat_col; row++)
+            for (int row = 0; row < Mat_col; row++)
             {
-                for(int colum = 0; colum < MatB_col; colum++)
+                for (int colum = 0; colum < MatB_col; colum++)
                 {
                     matricesB[i][row][colum] = rand() % 10;
                 }
@@ -61,24 +61,24 @@ int main(int argc, char **argv)
     }
     MPI_Barrier(MPI_COMM_WORLD);
     start_time = MPI_Wtime();
-    
-    //Local MatrixA and MatrixB declaration and distribute each process using Scatter function
-    int localMatrixA [Num_matrices / size][Mat_row][Mat_col];
-    int localMatrixB [Num_matrices / size][Mat_col][MatB_col];
+
+    // Local MatrixA and MatrixB declaration and distribute each process using Scatter function
+    int localMatrixA[Num_matrices / size][Mat_row][Mat_col];
+    int localMatrixB[Num_matrices / size][Mat_col][MatB_col];
     int result[Num_matrices / size][Mat_row][MatB_col];
 
-    MPI_Scatter(matricesA,(Num_matrices/size) * Mat_row * Mat_col,MPI_INT,localMatrixA,(Num_matrices/size) * Mat_row * Mat_col,MPI_INT,root,MPI_COMM_WORLD);
-    MPI_Scatter(matricesB,(Num_matrices/size) * Mat_col * MatB_col,MPI_INT,localMatrixB,(Num_matrices/size) * Mat_col * MatB_col,MPI_INT,root,MPI_COMM_WORLD);
+    MPI_Scatter(matricesA, (Num_matrices / size) * Mat_row * Mat_col, MPI_INT, localMatrixA, (Num_matrices / size) * Mat_row * Mat_col, MPI_INT, root, MPI_COMM_WORLD);
+    MPI_Scatter(matricesB, (Num_matrices / size) * Mat_col * MatB_col, MPI_INT, localMatrixB, (Num_matrices / size) * Mat_col * MatB_col, MPI_INT, root, MPI_COMM_WORLD);
 
-    //Matrix multiplication on each core
-    for(int i = 0; i < Num_matrices / size; i++)
+    // Matrix multiplication on each core
+    for (int i = 0; i < Num_matrices / size; i++)
     {
-        for(int j = 0; j < Mat_row; j++)
+        for (int j = 0; j < Mat_row; j++)
         {
-            for(int k = 0; k < MatB_col; k++)
+            for (int k = 0; k < MatB_col; k++)
             {
                 result[i][j][k] = 0;
-                for(int l = 0; l < Mat_col; l++)
+                for (int l = 0; l < Mat_col; l++)
                 {
                     result[i][j][k] += localMatrixA[i][j][l] * localMatrixB[i][l][k];
                 }
@@ -87,29 +87,30 @@ int main(int argc, char **argv)
     }
     end_time = MPI_Wtime();
     MPI_Barrier(MPI_COMM_WORLD);
-    printf("Process %d take time = %f second\n",rank, end_time - start_time);
+    printf("Process %d take time = %f second\n", rank, end_time - start_time);
 
-    //Answeres are gathered
-    int gatherMatrix [Num_matrices][Mat_row][MatB_col];
-    MPI_Gather(result,(Num_matrices / size) * Mat_row * MatB_col,MPI_INT, gatherMatrix,(Num_matrices / size) * Mat_row * MatB_col,MPI_INT,root,MPI_COMM_WORLD);
+    // Answeres are gathered
+    int gatherMatrix[Num_matrices][Mat_row][MatB_col];
+    MPI_Gather(result, (Num_matrices / size) * Mat_row * MatB_col, MPI_INT, gatherMatrix, (Num_matrices / size) * Mat_row * MatB_col, MPI_INT, root, MPI_COMM_WORLD);
 
-    //Print the resultant matrices
-    if(rank == root)
+    // Print the resultant matrices
+    if (rank == root)
     {
-        cout<<"All "<<Num_matrices<<" are :"<<endl;
-        for(int i = 0; i < Num_matrices; i++)
+        cout << "All " << Num_matrices << " are :" << endl;
+        for (int i = 0; i < Num_matrices; i++)
         {
-            cout<<"Matrix "<<i<<": "<<endl;
-            for(int j = 0; j < Mat_row; j++)
+            cout << "Matrix " << i << ": " << endl;
+            for (int j = 0; j < Mat_row; j++)
             {
-                for(int k = 0; k < MatB_col; k++)
+                for (int k = 0; k < MatB_col; k++)
                 {
-                    cout<<gatherMatrix[i][j][k]<<" ";
+                    cout << gatherMatrix[i][j][k] << " ";
                 }
-                cout<<endl;
+                cout << endl;
             }
+            cout << endl;
         }
     }
-      MPI_Finalize();
-      return 0;
+    MPI_Finalize();
+    return 0;
 }
---
+++ MPI/PhoneBook.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+#include <mpi.h>
+
+using namespace std;
+
+//... To compile: mpic++ phonebook-search.cpp -o phonebook-search
+//... To run: mpirun -n 4 ./phonebook-search phonebook1.txt phonebook2.txt
+
+void send_string(string text, int receiver)
+{
+    int length = text.size() + 1;
+    MPI_Send(&length, 1, MPI_INT, receiver, 1, MPI_COMM_WORLD);
+    MPI_Send(&text[0], length, MPI_CHAR, receiver, 1, MPI_COMM_WORLD);
+}
+
+string receive_string(int sender)
+{
+    int length;
+    MPI_Recv(&length, 1, MPI_INT, sender, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    char *text = new char[length];
+    MPI_Recv(text, length, MPI_CHAR, sender, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    return string(text);
+}
+
+string vector_to_string(vector<string> &words, int start, int end)
+{
+    string text = "";
+    for (int i = start; i < min((int)words.size(), end); i++)
+    {
+        text += words[i] + "\n";
+    }
+    return text;
+}
+
+vector<string> string_to_vector(string text)
+{
+    stringstream x(text);
+    vector<string> words;
+    string word;
+    while (x >> word)
+    {
+        words.push_back(word);
+    }
+    return words;
+}
+
+void check(string name, string phone, string search_name, int rank)
+{
+    if (name.size() != search_name.size())
+    {
+        return;
+    }
+    for (int i = 0; i < search_name.size(); i++)
+    {
+        if (name[i] != search_name[i])
+        {
+            return;
+        }
+    }
+    printf("%s %s found by process %d.\n", name.c_str(), phone.c_str(), rank);
+}
+
+void read_phonebook(vector<string> &file_names, vector<string> &names, vector<string> &phone_numbers)
+{
+    for (auto file_name : file_names)
+    {
+        ifstream file(file_name);
+        string name, number;
+        while (file >> name >> number)
+        {
+            names.push_back(name);
+            phone_numbers.push_back(number);
+        }
+        file.close();
+    }
+}
+
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int world_size, world_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    string search;
+    double start_time = MPI_Wtime();
+
+    if (!world_rank)
+    {
+        vector<string> names, phone_numbers;
+        vector<string> file_names(argv + 1, argv + argc);
+        read_phonebook(file_names, names, phone_numbers);
+        int segment = names.size() / world_size + 1;
+        cout << "Give name to search: ";
+        cin >> search;
+        for (int i = 1; i < world_size; i++)
+        {
+            int start = i * segment, end = start + segment;
+            string names_to_send = vector_to_string(names, start, end);
+            send_string(names_to_send, i);
+            string phone_numbers_to_send = vector_to_string(phone_numbers, start, end);
+            send_string(phone_numbers_to_send, i);
+            send_string(search, i);
+        }
+
+        for (int i = 0; i < segment; i++)
+        {
+            check(names[i], phone_numbers[i], search, world_rank);
+        }
+    }
+    else
+    {
+        string received_names = receive_string(0);
+        vector<string> names = string_to_vector(received_names);
+        string received_phone_numbers = receive_string(0);
+        vector<string> phone_numbers = string_to_vector(received_phone_numbers);
+        search = receive_string(0);
+
+        for (int i = 0; i < names.size(); i++)
+        {
+            check(names[i], phone_numbers[i], search, world_rank);
+        }
+    }
+
+    double finish_time = MPI_Wtime();
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    printf("Process %d took %f seconds.\n", world_rank, finish_time - start_time);
+
+    MPI_Finalize();
+
+    return 0;
+}
